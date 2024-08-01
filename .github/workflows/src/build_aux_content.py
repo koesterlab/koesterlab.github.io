8e01dad9c98d25889437bc4aaf1a52e7fc342762
Johannes Koester <johannes.koester@uni-due.de>
2024-08-01 09:10:00 +0200

    use git command

--- .github/workflows/src/build_aux_content.py
+++ .github/workflows/src/build_aux_content.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import yaml
 import subprocess as sp
-from git import Repo
 import shutil
 
 aux_content = yaml.safe_load(open(Path(__file__) / "aux_content.yml"))
@@ -10,7 +9,7 @@ for repo in aux_content:
     repo_name = Path(repo).basename()
     repo_dir: Path = Path("build") / repo_name
     repo_dir.mkdir(parents=True, exist_ok=True)
-    Repo.clone_from(f"https://github.com/{repo}", repo_dir)
+    sp.call(["git", "clone", f"https://github.com/{repo}", repo_dir], check=True)
 
     sp.call(["pixi", "run", "build"], check=True, cwd=repo_dir)
     teaching_dir = Path("src/teaching")
